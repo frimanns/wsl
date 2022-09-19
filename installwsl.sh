bdf3eb34eea0e53c5f605258b49cf0e41dbb7ac9
Jens-Ole Frimann <jens.ole.frimann@gmail.com>
2022-09-19 09:40:30 +0200

    Update installwsl.sh

--- installwsl.sh
+++ installwsl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo.
+echo .
 echo This will uninstall any previous Ubuntu-18.04 and install a new one.
 echo The new Ubutnu 18.04 will then create a new JupyterLab LXD container
 echo which will host Jupyter in Windows browser at http://localhost:8888.
-echo.
-echo.
+echo .
+echo .
